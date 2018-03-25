27931e73ef6f87360725dc03f9c5c10d08cab47d
k12club <k12club@hotmail.com>
2018-03-25 07:30:37 +0700

    templateupload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AppfooterComponent } from './components/appfooter/appfooter.component';
+import { AppheaderComponent } from './components/appheader/appheader.component';
+import { ApploginComponent } from './components/applogin/applogin.component';
+import { AppcontentComponent } from './components/appcontent/appcontent.component';
+import { AppbuttonComponent } from './components/appbutton/appbutton.component';
+import { AppregisterComponent } from './components/appregister/appregister.component';
+import { AppsettingComponent } from './components/appsetting/appsetting.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppfooterComponent,
+    AppheaderComponent,
+    ApploginComponent,
+    AppcontentComponent,
+    AppbuttonComponent,
+    AppregisterComponent,
+    AppsettingComponent,
+    
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/appbutton/appbutton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appbutton',
+  templateUrl: './appbutton.component.html',
+  styleUrls: ['./appbutton.component.css']
+})
+export class AppbuttonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
